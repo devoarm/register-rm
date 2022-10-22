05b0c47928a29266b11dcff666574d73f8574101
devoarm <nattapongarm2017@gmail.com>
2022-10-23 01:18:34 +0700

    edit

--- config/db_rm.js
+++ config/db_rm.js
@@ -1,16 +1,16 @@
 const knex = require("knex")({
   client: "mysql2",
   connection: {
-    // host: "192.168.3.229",
-    // port: 3306,
-    // user: "queue",
-    // password: "aranFvg8zjkowfh",
-    // database: "db_rm",
-    host: "localhost",
+    host: "192.168.3.229",
     port: 3306,
-    user: "root",
-    password: "",
+    user: "queue",
+    password: "aranFvg8zjkowfh",
     database: "db_rm",
+    // host: "localhost",
+    // port: 3306,
+    // user: "root",
+    // password: "",
+    // database: "db_rm",
   },
 });
 
--- config/utils.js
+++ config/utils.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://your_deployment.server.com';
+export const server = dev ? 'http://localhost:3000' : 'http://192.168.3.30:5503';
 // export const server = 'http://localhost:3000' ;
 
